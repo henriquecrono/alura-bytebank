a81b1a88dacf0dc288180a5867fd44dff9df9e41
henriquecrono <henriquemiranda5@hotmail.com>
2020-04-15 00:46:11 -0300

    screen dashboard to file; 'Transfers' card menu in dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:bytebank/screens/transferencia/list.dart';
+import 'package:bytebank/screens/dashboard/dashboard.dart';
 
 void main() => runApp(ByteBank());
 
@@ -22,56 +22,7 @@ class ByteBank extends StatelessWidget {
           textTheme: ButtonTextTheme.primary,
         ),
       ),
-      // home: TransfersList(),
       home: Dashboard(),
-    );
-  }
-}
-
-class Dashboard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Dashboard')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/bytebank_logo.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 100,
-              width: 150,
-              color: Theme.of(context).primaryColor,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Icon(
-                      Icons.people,
-                      color: Colors.white,
-                      size: 24,
-                    ),
-                    Text(
-                      'Contacts',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/screens/dashboard/dashboard.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'package:bytebank/components/card_menu.dart';
+
+class Dashboard extends StatelessWidget {
+  static const _appBarTitle = 'Dashboard';
+
+  static const _srcLogoByteBank = 'images/bytebank_logo.png';
+
+  static const _contactsCardIcon = Icons.people;
+  static const _contactsCardTitle = 'Contacts';
+
+  static const _transferListCardIcon = Icons.attach_money;
+  static const _transferListCardTitle = 'Transfers';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(_appBarTitle)),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(_srcLogoByteBank),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                CardMenu(
+                  cardIcon: _transferListCardIcon,
+                  cardTitle: _transferListCardTitle,
+                ),
+                CardMenu(
+                  cardIcon: _contactsCardIcon,
+                  cardTitle: _contactsCardTitle,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
