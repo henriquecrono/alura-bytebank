50354c6b41bf0eb757030e241c3942882725d12f
henriquecrono <henriquemiranda5@hotmail.com>
2020-04-15 00:20:40 -0300

    screen dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class ByteBank extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: _appName,
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        // primarySwatch: Colors.green,
+        primaryColor: Colors.green[900],
         // brightness: Brightness.dark,
         accentColor: Colors.blueAccent[700],
         buttonTheme: ButtonThemeData(
@@ -21,7 +22,56 @@ class ByteBank extends StatelessWidget {
           textTheme: ButtonTextTheme.primary,
         ),
       ),
-      home: TransfersList(),
+      // home: TransfersList(),
+      home: Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Dashboard')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 100,
+              width: 150,
+              color: Theme.of(context).primaryColor,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(
+                      Icons.people,
+                      color: Colors.white,
+                      size: 24,
+                    ),
+                    Text(
+                      'Contacts',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
